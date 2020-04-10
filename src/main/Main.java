15f5cc88c67d3ca2fcf9997b038b2afc3673751d
Arvind2105 <arvind.tomar_cs18@gla.ac.in>
2020-04-10 20:21:22 +0530

    create a void method and its definition....

--- src/main/Main.java
+++ src/main/Main.java
@@ -33,5 +33,15 @@ public class Main {
             System.out.printf("%d!\n", i);
         }
     }
+
+    static void test() {
+        System.out.println("Let's test your programming knowledge.");
+        // write your code here
+        System.out.println("Why do we use methods?");
+        System.out.println("1. To repeat a statement multiple times.");
+        System.out.println("2. To decompose a program into several small subroutines.");
+        System.out.println("3. To determine the execution time of a program.");
+        System.out.println("4. To interrupt the execution of a program.");
+    }
 }
 
